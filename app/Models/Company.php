d4e30c83647185d73f12edf83d45446f674e1397
naziabi <naziabi.hawaldar@cdpindia.com>
2022-11-05 21:41:41 +0530

    Many to Many relationship defined in User and Company model

--- app/Models/Company.php
+++ app/Models/Company.php
@@ -3,8 +3,13 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Company extends Model
 {
-    //
+    use SoftDeletes;
+    public function users()
+    {
+        return $this->belongsToMany(User::class, 'company_user');
+    }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -3,10 +3,15 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 use Laratrust\Traits\LaratrustUserTrait;
 
 class User extends Model
 {
     use LaratrustUserTrait;
-    //
+    use SoftDeletes;
+    public function companies()
+    {
+        return $this->belongsToMany(Company::class, 'company_user');
+    }
 }
